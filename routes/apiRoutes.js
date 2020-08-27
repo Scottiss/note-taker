d579c39bec1d5bf919631a5eddd55998d52ae642
Scott Lagas <scottlagas@outlook.com>
2020-08-26 20:27:03 -0400

    all files updated

---
+++ routes/apiRoutes.js
@@ -0,0 +1,106 @@
+/* routes for the api */
+const path = require("path");
+const fs = require("fs");
+
+const checkObject = require("../utils/checkObject.js");
+
+const databaseDir = path.join(__dirname, "../", "db");
+
+module.exports = function(app) {
+  // route to send the notes
+  app.get("/api/notes", (req, res) => {
+    let json;
+    let data = fs.readFileSync(path.join(databaseDir, "db.json"), "utf8");
+    if (data) {
+      json = JSON.parse(data);
+    } else {
+      json = { msg: "No notes Found"};
+    }
+    
+    res.json(json);
+  })
+
+
+  // post note to api
+  app.post("/api/notes", (req, res) => {
+    let data = req.body; //uses express.json middleware and parses json to javascript object for you
+    let fileData = fs.readFileSync(path.join(databaseDir, "db.json"), "utf8");
+    let dbData = JSON.parse(fileData); // array of objects
+
+    // get object length (get last objects id which is the largest up to that point. then add 1)
+    let id;
+    if (dbData.length === 0) {
+      id = 1; 
+    } else {
+      id = parseInt(dbData[dbData.length - 1].id) + 1; 
+    }
+
+    // if JSON array of objects is sent
+    if (Array.isArray(data)) {
+      //console.log("got an array")
+
+      // check that every object is sent in correct format
+      if (!data.every(checkObject)) {
+        res.json({error: "obj only contain text and title property"});
+        return;
+      }
+      
+      data.forEach(elem => { 
+        elem.id = id;
+        dbData.push(elem);
+        id++;
+      })
+      
+      // write to file
+      fs.writeFile(path.join(databaseDir, "db.json"), JSON.stringify(dbData), (err) => {
+        if(err) console.log("can't write to file")
+        //console.log("file written to");
+      })
+
+      res.json(data) 
+      return
+    } else {
+      if (checkObject(data)) {
+        data.id = id;
+        dbData.push(data);
+        
+        // write to file
+        fs.writeFile(path.join(databaseDir, "db.json"), JSON.stringify(dbData), (err) => {
+          if(err) console.log("can't write to file")
+          //console.log("file written to");
+        });
+        res.json(data);
+      } else {
+        res.json({error: "Can't post to database"});
+      }
+    }
+    
+    return
+  })
+
+
+
+  // delete note api
+  // delete note variable parameter
+  app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id; 
+    
+    let dbData = fs.readFileSync(path.join(databaseDir, "db.json"), "utf8");
+    dbData = JSON.parse(dbData);
+    const note = dbData.filter(note => note.id == parseInt(id))[0]; 
+    const index = dbData.indexOf(note);
+    
+    // indexOf returns -1 if not part of array
+    if (index > -1) {
+      dbData.splice(index, 1);
+      fs.writeFileSync(path.join(databaseDir, "db.json"), JSON.stringify(dbData));
+      res.json(note);
+    } else {
+      res.json({error: `No note with id ${id} found`});
+    }
+
+    return
+  })
+
+ 
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// this middleware is used for parsing request body found in POST and PUT requests
+app.use(express.json()); // for content-type: application/json  
+app.use(express.urlencoded({ extended: false })); // for content-type: application/x-www-form-urlencoded (form data) no embedded object, string or array
+
+//static file serving so you don't have to keep making routes for css, js etc
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// api routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on Port ${PORT}`);
+})
+
+
+
+
+
